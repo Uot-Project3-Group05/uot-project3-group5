5ee2b2d74afc56a76420f7b9ef441de7ede5b850
anagha778 <anagha778@gmail.com>
2021-05-11 15:57:28 -0400

    card schema

---
+++ server/models/Card.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const cardSchema = new Schema({
+    
+    cardId: {
+        type: Number,
+        required: true,
+      },
+
+      front:[
+        {
+        type: String,
+        required: true
+         }
+      ],
+      
+      back : [
+          {
+        type: String,
+        required: true      
+          }
+      ]     
+});
+
+const Card = mongoose.model('Card', cardSchema);
+
+module.exports = Card;
