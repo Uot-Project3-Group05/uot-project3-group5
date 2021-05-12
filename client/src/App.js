fe58f059646d16f4f0613f55740af5544a732278
esroleo <esroleo@gmail.com>
2021-05-11 22:43:52 -0400

    front end main page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,12 @@ import {
   Code,
   Grid,
   theme,
+  Flex,
+  Spacer, 
+  Avatar,
+  Wrap, 
+  WrapItem,
+  Center
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
@@ -15,9 +21,68 @@ import { Logo } from './Logo';
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
+      <Box textAlign="center" fontSize="xl" >
+        {/*Start of Header - move to header component*/}
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <Flex >
+            <Box p="4" >
+              Logo
+            </Box>
+            <Spacer />
+            <Box p="4">
+              {/*<Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />*/}
+              <Avatar ></Avatar>
+            </Box>
+        </Flex>
+        {/*End of Header*/}
+
+        <Box textAlign="center" fontSize="xl" mb={6} >
+          Pick a Deck to Begin!
+        </Box>
+
+     
+        
+        <Wrap  justify="space-evenly" spacing="8" >
+          <WrapItem boxShadow="2xl">
+            <Center w="300px" h="300px" bg="red.200">
+              Box 1
+            </Center>
+          </WrapItem>
+          <WrapItem boxShadow="2xl"> 
+            <Center w="300px" h="300px" bg="green.200">
+              Box 2
+            </Center>
+          </WrapItem>
+          <WrapItem boxShadow="2xl"> 
+            <Center w="300px" h="300px" bg="blue.200">
+              Box 3
+            </Center>
+          </WrapItem>
+          <WrapItem boxShadow="2xl"> 
+            <Center w="300px" h="300px" bg="yellow.200">
+              Box 4
+            </Center>
+          </WrapItem>
+          <WrapItem boxShadow="2xl"> 
+            <Center w="300px" h="300px" bg="orange.200">
+              Box 5
+            </Center>
+          </WrapItem>
+          <WrapItem boxShadow="2xl"> 
+            <Center w="300px" h="300px" bg="purple.200">
+              Box 6
+            </Center>
+          </WrapItem>
+        </Wrap>
+       
+
+        
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+/* column flex default sample
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
             <Text>
@@ -33,10 +98,17 @@ function App() {
               Learn Chakra
             </Link>
           </VStack>
+
+*/
+
+/* example of grid
+
+        <Grid minH="100vh" p={3}>
+
+         
+
         </Grid>
-      </Box>
-    </ChakraProvider>
-  );
-}
+
+*/
 
 export default App;
