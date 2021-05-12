f06f87e2598a380a2b57b46dc8eb049ff2778d04
a-taghva <taghvali99@gmail.com>
2021-05-12 18:39:52 -0400

    model typeDefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,29 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    badget: [Object]
+    deck: Deck
+  }
+
+  type Card {
+    cardId: ID
+  }
+
+  type Deck {
+    _id: ID
+    deckname: Stirng
+    description: String
+    cards: [Card]
+  }
+
+  type Game {
+    username: String
+    score: Int
+    deck: [Deck]
+  }
+`;
