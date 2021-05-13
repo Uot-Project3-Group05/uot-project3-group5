d5dfb51874b25bf9566cbe66c8683585119d958d
anagha778 <anagha778@gmail.com>
2021-05-12 23:06:04 -0400

    added game ref in user model

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 const bcrypt = require('bcrypt');
+const gameSchema = require('./Game');
 
 const userSchema = new Schema({
     username: {
@@ -26,11 +27,7 @@ const userSchema = new Schema({
           type: String,
         },
       ],
-      deck : {
-        type: Schema.Types.ObjectId,
-        ref: 'Deck',
-        required: true
-      }    
+      games : [[gameSchema]]
 });
 
 // set up pre-save middleware to create password
