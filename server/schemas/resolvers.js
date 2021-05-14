ff58b6bf06be3f3630b0cf0f70b190418a9d529e
Mateo <mtaycruz@gmail.com>
2021-05-14 18:50:16 -0400

    updated resolvers and typedefs

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,33 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+const resolvers = {
+  Query: {
+    me: async (parent, params, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+        .select('-__v -password');
+        return userData;
+      }
+    }
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('invalid credentials!');
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('invalid credentials!');
+      }
+      const token = signToken(user);
+      return { token, user };
+    }
+  }
+}
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -31,6 +31,15 @@ const typeDefs = gql`
       back: [String]
 
   }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    addUser($username: String!, $email: String!, password: String!): Auth
+    login($username: String!, password: String!): Auth
+  }
+
+
 `;
 
 // export the typeDefs
