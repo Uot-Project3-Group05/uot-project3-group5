16b26d9e93a877857e44cf96653c5882b55f07ee
Mateo <mtaycruz@gmail.com>
2021-05-14 22:35:30 -0400

    updated the resolvers and typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    badges: [String]
+    deck:[Deck]
+  }
+  type Game {
+    _id: ID
+    username: String
+    score: Int
+    deck: [Deck]
+  }
+  type Deck {
+    _id: ID
+    deckname: String
+    description: String
+    cards: [Card]
+  }
+  type Card {
+      _id: ID
+      front:[String]
+      back: [String]
+  }
+  type Query {
+    me: User
+    users:[User]
+    user(username: String!): User
+  }
+  type Mutation {
+    addUser($username: String!, $email: String!, password: String!): Auth
+    login($username: String!, password: String!): Auth
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
