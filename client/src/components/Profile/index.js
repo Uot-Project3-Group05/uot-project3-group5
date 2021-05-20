10abe246565cd3b5677c5e436e972f0246f03363
a-taghva <taghvali99@gmail.com>
2021-05-20 18:12:47 -0400

    setup /profile/:username? logic

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,32 +1,59 @@
 import React from 'react';
-import { Link, useParams } from "react-router-dom";
+import { Link, Redirect, useParams } from "react-router-dom";
+
 import {
   Box,
-  Flex,
-  Spacer,
-  Avatar,
   Wrap,
   WrapItem,
   Center,
   SimpleGrid,
   Image,
   Progress,
-  CircularProgress,
-  CircularProgressLabel
 } from '@chakra-ui/react';
 
 import { 
   GiEmerald,
   GiGoldBar
- } from "react-icons/gi";
- 
- import periodicTable from '../../assets/images/periodic-table.svg'
- import flagsWorld from '../../assets/images/flags-world.png'
+} from "react-icons/gi";
 
+import { useQuery } from '@apollo/react-hooks';
+import { ME, GET_USER_BY_USERNAME } from '../../utils/queries';
+import Auth from '../../utils/auth';
 
+import periodicTable from '../../assets/images/periodic-table.svg'
+import flagsWorld from '../../assets/images/flags-world.png'
 
 
 function Profile() {
+  const { username } = useParams();
+  // const { loading: isLoading, data: isData } = useQuery(ME);
+
+  if (!username && !Auth.loggedIn()) {
+    return <h1>please use the navigation to login!!</h1>
+  }
+
+  // isData && console.log(isData);
+
+  const { loading, data } = useQuery(username ? GET_USER_BY_USERNAME : ME, {
+    variables: { username }
+  })
+
+  if (loading) {
+    return <h1>loading...</h1>
+  }
+
+  const user = data.me || data.user || false;
+
+  if (!user) {
+    return <h1>404 NOT FOUND!!</h1>
+  }
+
+  // const user = data?.me || data?.user || [];
+  if (Auth.loggedIn() && Auth.getProfile().data.username === username) {
+    return <Redirect to="/profile" />;
+  }
+
+  console.log(user);
 
   const deck = [
     
