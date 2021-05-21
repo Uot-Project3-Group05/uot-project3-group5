f5931d592d1b267acb5c9178d117c42da1cead02
bluesatyr <shawnevans.music@gmail.com>
2021-05-21 17:50:58 -0400

    added International Flag Deck to seeds

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -2,11 +2,13 @@ const db = require('../config/connection');
 const { Deck, Card } = require('../models');
 
 const DECK_DATA = require('./data');
+const DECK2_DATA = require('./flagdata');
 
 db.once('open', async () => {
   await Deck.deleteMany({});
 
   await Deck.collection.insertOne(DECK_DATA);
+  await Deck.collection.insertOne(DECK2_DATA);
 
   console.log('all done!');
   process.exit(0);
